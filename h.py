21c47141052f3025333758456c15ca6ac97ffd76
A-web-web <thornyno.gmail.com>
2025-09-21 05:52:55 -0700

    hello

---
+++ h.py
@@ -0,0 +1,16 @@
+a = input("Enter a word you want to encrypt: ")
+d = int(input("Enter a dhift number: "))
+b = a.lower
+for i in a:
+    if i.isalpha():
+        n = ord(i) + d
+        if n > ord('z'):
+            n -= 26
+        print(chr(n), end='')
+    else:
+        print(i, end='')
+
+
+
+
+
---
+++ python.py
@@ -0,0 +1,14 @@
+a = input("Enter a word you want to decrypt: ")
+a = input("Enter a word you want to decrypt: ")
+d = int(input("Enter a dhift number: "))
+b = a.lower
+for i in a:
+    if i.isalpha():
+        n = ord(i) - d
+        if n > ord('z'):
+            n += 26
+        print(chr(n), end='')
+    else:
+        print(i, end='')
+
+
